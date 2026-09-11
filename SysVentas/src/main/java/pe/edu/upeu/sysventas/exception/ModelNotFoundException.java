121120008e4dcab33cf866ce34a8294345ded7df
Pixelstorn <gabriel.vilca@upeu.edu.pe>
2026-09-11 12:48:31 -0500

    Segundo avance

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/exception/ModelNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.sysventas.exception;
-
-import pe.edu.upeu.sysventas.model.Proveedor;
-
-public class ModelNotFoundException extends RuntimeException {
-    public ModelNotFoundException(String message) {
-        super(message);
-    }
-}
